671cc6a6230e9b6641203dee35a43a60f1e33399
omer.alaca <omer.alaca@kron.com.tr>
2021-02-09 05:33:11 +0300

    cors configuration is completed.

--- app.js
+++ app.js
@@ -8,8 +8,20 @@ const swaggerFile = require(config.apidoc.outputfile);
 app.get("/test", (req, res) =>
   res.send("I'm the first endpoint for Numbly was developed!")
 );
+app.get("/test2", (req, res) => res.send(config.web.host));
 app.listen(config.web.port, () => {
   console.log("Listening on http://" + config.web.host + config.web.port);
 });
 
 app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});

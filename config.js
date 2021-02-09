c37d983cd5af0b92d72209704177d50032e3eed2
omer.alaca <omer.alaca@kron.com.tr>
2021-02-09 05:43:47 +0300

    local dev config update.

--- config.js
+++ config.js
@@ -1,7 +1,12 @@
 var config = {};
 
 config.web = {};
-config.web.host = process.env.IP || "localhost:";
+if (process.env.PORT == undefined) {
+  config.web.host = "localhost:8000";
+} else {
+  config.web.host = "numbly-api.herokuapp.com";
+}
+
 config.web.port = process.env.PORT || 8000;
 
 config.apidoc = {};
@@ -14,8 +19,8 @@ config.apidoc.description = {
     description:
       "Greetings dear developer! This is the all-in api of the number-based guessing game called Numbly you have seen. Enjoy it!",
   },
-  host: "numbly-api.herokuapp.com",
-  schemes: ["https"],
+  host: config.web.host,
+  schemes: ["https", "http"],
 };
 
 module.exports = config;
